39454d5686a7a7361bcdfb15528c0e682f36ba8d
alyam07 <anikaan07@gmail.com>
2025-07-17 17:52:11 +0300

    script.js

--- script.js
+++ script.js
@@ -60,7 +60,7 @@ function drawPlayer() {
 
 function drawBones() {
   bones.forEach(b => {
-    ctx.drawImage(boneImg, lanes[b.lane], b.y, 80, 80);
+    ctx.drawImage(boneImg, lanes[b.lane], b.y, 75, 75);
   });
 }
 
